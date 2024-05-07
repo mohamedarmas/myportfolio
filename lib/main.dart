02e63713c6ecc43e96375e962dfa98115e497ff3
Felipe Sales <soufeliposales@gmail.com>
2024-05-07 19:05:50 -0300

    chore: removed runZonedGuarded for prevent twice on app

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,23 @@
 import 'dart:async';
-import 'dart:developer';
 
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-
 import 'package:url_strategy/url_strategy.dart';
 
 import 'package:site/app/app_widget.dart';
 import 'package:site/app/core/injections/injections.dart';
 import 'package:site/data/services/firebase/firebase.dart';
-// import 'package:site/data/services/recaptcha/recaptcha.dart';
 
 Future<void> main() async {
-  await runZonedGuarded(() async {
-    WidgetsFlutterBinding.ensureInitialized();
-    await FirebaseServiceImpl().setUpInitialization();
+  WidgetsFlutterBinding.ensureInitialized();
+  await FirebaseServiceImpl().setUpInitialization();
 
-    // if (kIsWeb) {
-    //   await RecaptchaService.initiate();
-    // }
+  // if (kIsWeb) {
+  //   await RecaptchaService.initiate();
+  // }
 
-    debugPrint('main: runZonedGuarded 2');
-    setPathUrlStrategy();
-    configureDependencies();
-    runApp(
-      AppWidget(),
-    );
-  }, (error, stackTrace) {
-    log('runZonedGuarded: Caught error: $error');
-    log('runZonedGuarded: StackTrace: $stackTrace');
-  });
+  setPathUrlStrategy();
+  configureDependencies();
+  runApp(
+    AppWidget(),
+  );
 }
