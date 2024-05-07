0cfa23ca99a68c6559c29c6812a385c698dabc49
Felipe Sales <soufeliposales@gmail.com>
2024-05-07 19:33:59 -0300

    refactor: removed help method

--- lib/app/features/home/widgets/contact/contact_widget.dart
+++ lib/app/features/home/widgets/contact/contact_widget.dart
@@ -12,7 +12,7 @@ import 'package:site/app/features/home/widgets/contact/widgets/widgets.dart';
 import 'package:site/app/widgets/snack_bars/snack_bars.dart';
 import 'package:site/data/models/models.dart' as models;
 import 'package:site/data/repositories/contact/contact.dart';
-import 'package:site/data/services/recaptcha/recaptcha.dart';
+// import 'package:site/data/services/recaptcha/recaptcha.dart';
 
 class ContactWidget extends StatelessWidget {
   ContactWidget({
@@ -34,66 +34,61 @@ class ContactWidget extends StatelessWidget {
     final emailController = TextEditingController();
     final messageController = TextEditingController();
     final subjectController = TextEditingController();
+    final form = CustomForm(
+      formKey: formKey,
+      nameController: nameController,
+      emailController: emailController,
+      subjectController: subjectController,
+      messageController: messageController,
+      onPressed: () async {
+        if (formKey.currentState?.validate() ?? false) {
+          // final isNotABot = await RecaptchaService.isNotABot();
 
-    Widget contactForm() {
-      return CustomForm(
-        formKey: formKey,
-        nameController: nameController,
-        emailController: emailController,
-        subjectController: subjectController,
-        messageController: messageController,
-        onPressed: () async {
-          if (formKey.currentState?.validate() ?? false) {
-            // final isNotABot = await RecaptchaService.isNotABot();
-
-            // if (isNotABot) {
-            if (true) {
-              if (context.mounted) {
-                appShowSnackBar(
-                  context,
-                  text: AppTexts.get(context).emailSendedWithSuccess,
-                  icon: Icons.check,
-                  color: AppColors.primaryDark,
-                  width: 300,
-                );
-              }
-
-              _contactController?.sendMail(
-                contact: models.Contact(
-                  name: nameController.text,
-                  email: emailController.text.trim(),
-                  message: messageController.text,
-                  subject: subjectController.text,
-                ),
-              );
-
-              for (var controller in [
-                nameController,
-                emailController,
-                messageController,
-                subjectController,
-              ]) {
-                controller.clear();
-              }
-            } else if (context.mounted) {
+          // if (isNotABot) {
+          if (true) {
+            if (context.mounted) {
               appShowSnackBar(
                 context,
-                text: AppTexts.get(context).emailNotSended,
-                icon: Icons.error,
-                color: AppColors.red,
+                text: AppTexts.get(context).emailSendedWithSuccess,
+                icon: Icons.check,
+                color: AppColors.primaryDark,
                 width: 300,
               );
             }
+
+            _contactController?.sendMail(
+              contact: models.Contact(
+                name: nameController.text,
+                email: emailController.text.trim(),
+                message: messageController.text,
+                subject: subjectController.text,
+              ),
+            );
+
+            for (var controller in [
+              nameController,
+              emailController,
+              messageController,
+              subjectController,
+            ]) {
+              controller.clear();
+            }
+          } else if (context.mounted) {
+            appShowSnackBar(
+              context,
+              text: AppTexts.get(context).emailNotSended,
+              icon: Icons.error,
+              color: AppColors.red,
+              width: 300,
+            );
           }
-        },
-      );
-    }
+        }
+      },
+    );
 
     return LayoutBuilder(
       key: AppKeys.contact,
       builder: (context, constraints) {
-        final form = contactForm();
-
         return constraints.maxWidth < Breakpoints.contact
             ? ContactMobile(form)
             : ContactWeb(form);
