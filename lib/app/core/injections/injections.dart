8233fd8cba8e46a4c1ea0e74e45ddfaae701d751
Felipe Sales <soufeliposales@gmail.com>
2024-05-28 20:59:16 -0300

    chore: removed unused injections

--- lib/app/core/injections/injections.dart
+++ lib/app/core/injections/injections.dart
@@ -2,8 +2,6 @@ import 'package:dio/dio.dart';
 import 'package:firebase_remote_config/firebase_remote_config.dart';
 import 'package:site/app/features/contact/contact.dart';
 import 'package:get_it/get_it.dart';
-import 'package:site/data/services/firebase/firebase_service.dart';
-import 'package:site/data/services/firebase/firebase_service_impl.dart';
 
 final getIt = GetIt.I;
 
@@ -18,11 +16,6 @@ void configureDependencies() {
       FirebaseRemoteConfig.instance,
     );
   }
-  // if (!getIt.isRegistered<FirebaseService>()) {
-  //   getIt.registerSingleton<FirebaseService>(
-  //     FirebaseServiceImpl(),
-  //   );
-  // }
   if (!getIt.isRegistered<ContactRepository>()) {
     getIt.registerLazySingleton<ContactRepository>(
       () => ContactRepositoryImpl(
